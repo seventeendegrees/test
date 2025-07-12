40c0540c0051e5ceccb9c37e9f69319cefb8f28a
seventeendegrees <degreesof17@outlook.com>
2025-07-12 22:55:54 +0800

    Changes

---
+++ test006.py
@@ -0,0 +1,5 @@
+year=2099#定义年
+age=27#定义年龄
+
+str1="今年是{}年".format(year)+"，我现在是{}岁".format(age)
+print(str1)
---
+++ test066.py
@@ -0,0 +1,5 @@
+year=2099#定义年
+age=27#定义年龄
+
+str1="今年是%d年"%year+"，我现在是%d岁"%age
+print(str1)
